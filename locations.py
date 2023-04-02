18a0e6a216bbbf5a7972f301c1af2f4abecbc76e
Danny Deng <dannydeng@Dannys-MacBook-Air.local>
2023-04-02 11:01:35 -0700

    added mountain range location

--- locations.py
+++ locations.py
@@ -19,7 +19,7 @@ class Grasslands:
         self.guards = []
         self.enemies = [Bandit()]
         self.description = "Wide fields of grass with plenty of wildlife and few bandits."
-        self.connecting_locations = [StartTown, Forest]
+        self.connecting_locations = [StartTown, Forest, Mountain_Range]
 
 class Forest:
     def __init__(self):
@@ -27,10 +27,21 @@ class Forest:
         self.villagers = []
         self.merchants = []
         self.guards = []
-        self.enemies = [Bear(), Wolf()]
+        self.enemies = [Wolf()]
         self.description = "Towering trees covered with foliage hiding more dangerous wildlife."
-        self.connecting_locations = [Grasslands]
+        self.connecting_locations = [Grasslands, Mountain_Range]
 
+class Mountain_Range:
+    def __init__(self):
+        self.name = "The Frosty Peaks"
+        self.villagers = []
+        self.merchants = []
+        self.guards = []
+        self.enemies = [Bear()]
+        self.description = "A treacherous, snowy mountain range with bears that have awoken from slumber, and seeks food."
+        self.connecting_locations = [Grasslands, Forest]
+        
+        
 #all_locations = [StartTown, Grasslands, Forest]
 
 # Grasslands      # type object
